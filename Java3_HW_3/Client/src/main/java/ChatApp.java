cd50ae18e589a05eb2563d4e2ffce0444fd6f33f
JonnySPb127778 <aei.spb@gmail.com>
2021-03-15 13:17:09 +0300

    Java3 Wome Work 3

---
+++ Java3_HW_3/Client/src/main/java/ChatApp.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ChatApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("src/main/java/MyChat.fxml"));
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("My Chat");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
